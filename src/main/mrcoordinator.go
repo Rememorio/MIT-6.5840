31f75e623f59deb824bcf69cd0c3729b6f4d89d6
Rememorio <1126631928@qq.com>
2024-09-10 21:47:34 +0800

    refactor: go imports mrapps

--- src/main/mrcoordinator.go
+++ src/main/mrcoordinator.go
@@ -16,12 +16,12 @@ import "fmt"
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: mrcoordinator inputfiles...\n")
+		_, _ = fmt.Fprintf(os.Stderr, "Usage: mrcoordinator inputfiles...\n")
 		os.Exit(1)
 	}
 
 	m := mr.MakeCoordinator(os.Args[1:], 10)
-	for m.Done() == false {
+	for !m.Done() {
 		time.Sleep(time.Second)
 	}
 
